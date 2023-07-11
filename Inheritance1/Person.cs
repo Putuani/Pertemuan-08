14bad8c6462849aa42902d21c1ff51f3d1ea1533
Putuani <2022@DESKTOP-KE3SIRF>
2023-07-11 06:25:12 -0800

    Add project files.

---
+++ Inheritance1/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance1
+{
+    public class Person
+    {
+
+  
+       //Property
+       public string Name;
+       public int Age;
+
+       //constructor
+       public Person(string Name, int Age)
+       {
+            this.Name = Name;
+            this.Age = Age;
+       }
+
+       public Person()
+       {
+
+       }
+
+       //Fungsi
+       public void InfoPerson()
+       {
+            Console.WriteLine("Nama saya {0}, dan umur saya {0}, tahun", this.Name, this.Age);
+       }
+    }
+
+        // class turunan
+        public class karyawan : Person
+        {
+            //property class turunan
+            public string karyawanID { set; get; }
+            public string subject { set; get; }
+
+            //overide method
+            public void InfoPerson(string subject)
+            {
+                //this
+                this.subject = subject;
+                Console.WriteLine("Nama saya {0}, dan umur saya {0}, tahun", this.subject);
+            }
+        }
+
+    
+}
---
+++ Inheritance1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Name = "putu";
+            person1.Age = 19;
+            person1.InfoPerson();
+
+            //inthertance
+            karyawan karyawan1 = new karyawan();
+            karyawan1.Name = "putu";
+            karyawan1.Age = 20;
+            karyawan1.InfoPerson();
+            karyawan1.InfoPerson("Dosen");
+
+            Console.ReadKey();
+        }
+    }
+}
+        
